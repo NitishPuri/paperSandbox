4a28a43fcaec89ffb007a2fbb360bd9be26b7ebb
Nitish Puri <npuri1903@gmail.com>
2024-03-07 10:29:13 +0530

    add test for boolean ops with shapes

--- src/main.js
+++ src/main.js
@@ -5,14 +5,86 @@ window.onload = async function () {
   // Setup Paper.js with the canvas
   paper.setup(document.getElementById("myCanvas"));
 
-  paper.view.viewSize = new paper.Size(800, 400);
+  paper.view.viewSize = new paper.Size(1000, 400);
 
-  // Create a rectangle shaped path at the center of the view:
-  var path = new paper.Path.Rectangle({
-    center: paper.view.center,
-    size: new paper.Size(80, 50),
+  let circlePath = new paper.Path.Circle({
+    center: new paper.Point(70, 100),
+    radius: 50,
   });
+  // circlePath.fillColor = "red";
+  circlePath.strokeColor = "red";
 
-  // Fill the path with a color
-  path.fillColor = "black";
+  console.log("circlePath.area", circlePath.area);
+  // circlePath.fillColor =
+  // circlePath.remove();
+
+  let circlePath2 = new paper.Path.Circle({
+    center: new paper.Point(140, 100),
+    radius: 50,
+  });
+  // circlePath2.fillColor = "blue";
+  circlePath2.strokeColor = "blue";
+  console.log("circlePath2.area", circlePath2.area);
+  // circlePath2.remove();
+
+  let addPath = circlePath.clone();
+  addPath.translate(new paper.Point(0, 110));
+  let addPath2 = circlePath2.clone();
+  addPath2.translate(new paper.Point(0, 110));
+  addPath = addPath.unite(addPath2);
+  addPath2.remove();
+  console.log("addPath.area", addPath.area);
+  addPath.fillColor = "green";
+
+  let subtractPath = addPath.clone();
+  subtractPath.translate(new paper.Point(0, 110));
+  subtractPath.strokeColor = null;
+  subtractPath.fillColor = null;
+  let subtractPath2 = circlePath2.clone();
+  subtractPath2.translate(new paper.Point(0, 220));
+  subtractPath = subtractPath.subtract(subtractPath2);
+  subtractPath2.remove();
+  subtractPath.fillColor = "red";
+  subtractPath.strokeColor = "black";
+  console.log("subtractPath.area", subtractPath.area);
+
+  let subtractPath3 = addPath.clone();
+  subtractPath3.translate(new paper.Point(0, 220));
+  subtractPath3.strokeColor = null;
+  subtractPath3.fillColor = null;
+  let subtractPath4 = circlePath2.clone();
+  subtractPath4.translate(new paper.Point(0, 330));
+  subtractPath4.strokeColor = null;
+  let subtractPath5 = subtractPath4.subtract(subtractPath3);
+  subtractPath5.fillColor = "blue";
+  // let subtractPath6 = subtractPath3.subtract(subtractPath4);
+  // subtractPath6.fillColor = "green";
+  // subtractPath3 = subtractPath3.subtract(subtractPath4);
+  // subtractPath3.remove();
+  console.log("subtractPath5.area", subtractPath5.area);
+  // console.log("subtractPath6.area", subtractPath6.area);
+  // subtra
+
+  // let
+
+  // addPath.fillColor = "green";
+  // let addPath = circlePath.unite(circlePath2);
+  // addPath.remove();
+  // addPath.fillColor = "green";
+
+  // let circlePath3 = new paper.Path.Circle({
+  //   center: new paper.Point(210, 100),
+  //   radius: 50,
+  // });
+  // circlePath3.fillColor = "green";
+
+  // let subtractPath = circlePath.subtract(circlePath2, {
+  //   insert: true,
+  //   trace: true,
+  // });
+  // console.log(subtractPath);
+  // subtractPath.style.fillColor = "red";
+  // circlePath2.fillColor = "white";
+
+  paper.view.draw();
 };
